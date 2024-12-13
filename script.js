fc52534d8882873e230540c184975538ed822dcd
bhpuja <vpuja0421@gmail.com>
2024-12-13 17:40:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
 //your JS code here. If required.
+// Mapping of keycodes to sounds
+const soundMapping = {
+  65: 'sounds/a.mp3',
+  83: 'sounds/s.mp3',
+  68: 'sounds/d.mp3',
+  70: 'sounds/f.mp3',
+  71: 'sounds/g.mp3',
+  72: 'sounds/h.mp3',
+  74: 'sounds/j.mp3',
+  75: 'sounds/k.mp3',
+  76: 'sounds/l.mp3',
+};
+
+// Function to play sound based on the key pressed
+function playSound(keyCode) {
+  const sound = new Audio(soundMapping[keyCode]);
+  sound.play();
+}
+
+// Event listener for keydown events
+window.addEventListener('keydown', (e) => {
+  const keyCode = e.keyCode;
+  if (soundMapping[keyCode]) {
+    playSound(keyCode);
+  }
+});
+
+// Event listener for button clicks
+const keys = document.querySelectorAll('.key');
+keys.forEach((key) => {
+  key.addEventListener('click', () => {
+    const keyCode = key.getAttribute('data-key');
+    playSound(keyCode);
+  });
+});
